7adca5cbd51df06c87b84b0df6fd8302b5566f1d
GolovinVI <golovin.product@gmail.com>
2022-07-25 18:27:45 +0300

    PP_1.1.4(Edit 2)
    PP_1.1.4(Edit 2)

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+    private UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
     public void createUsersTable() {
         userDaoHibernate.createUsersTable();
     }
